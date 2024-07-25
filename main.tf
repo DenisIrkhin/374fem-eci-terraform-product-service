1900f0f67c7c7ea9f941bd5d3076a7ebb2ebd178
Denis Irkhin <devdenis555@gmail.com>
2024-07-25 19:16:51 -0400

    fix: files

--- main.tf
+++ main.tf
@@ -1,16 +1,14 @@
 module "service" {
-  # for_each = local.services
-
   source = "./module/ecs-service"
 
   cluster_name = var.cluster_name
   environment  = var.environment
   image        = "${var.registry}/fem-eci-service:${var.environment}"
   name         = "service"
-  # parameters   = each.value.parameters
-}
 
-# moved {
-#   from = module.service
-#   to   = module.service["service"]
-# }
+  parameters = [
+    "fem-instructor",
+    "fem-location",
+    "fem-version"
+  ]
+}
